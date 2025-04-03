eaed874d0b9109315c9f2719c3e653f320820d27
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-04-03 14:47:43 +0530

    dotnet core project day 2

--- EMS.Repositories/ApplicationDbContext.cs
+++ EMS.Repositories/ApplicationDbContext.cs
@@ -18,5 +18,8 @@ namespace EMS.Repositories
         public DbSet<Branch> Branches { get; set; }
 
         public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveApplication> LeaveApplications { get; set; }
     }
 }
